41ed2e761465e9d4700b70979e182da588a18154
Swaraj Papadkar <apple@Apples-MacBook-Pro.local>
2019-11-30 02:35:32 +0530

    settings.py

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todolist_app',
+    'todo_app',
     'users_app',
     'crispy_forms',
 ]
